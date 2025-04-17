85083d1735259d21a66867080854f6889ab44fe6
HShrigma <hristostantchev@abv.bg>
2025-04-17 13:14:27 +0300

    Add Naive DBFile tests

--- src/main/java/jankdb/DBFile.java
+++ src/main/java/jankdb/DBFile.java
@@ -1,23 +1,31 @@
 package jankdb;
 
+import java.io.*;
+
 public class DBFile {
     String label;
     String filePath;
+    String extension = ".txt";
+    final String DEFAULT_PATH_PREFIX = "src/main/resources/"; 
+    StringBuilder data;
 
     // Constructors ----------------------------------------
     public DBFile() {
         label = "";
         filePath = "";
+        data = new StringBuilder();
     }
 
     public DBFile(String label) {
         this.label = label;
         filePath = "";
+        data = new StringBuilder();
     }
 
     public DBFile(String label, String filePath) {
         this.label = label;
         this.filePath = filePath;
+        data = new StringBuilder();
     }
 
     // END Constructors ------------------------------------
@@ -37,5 +45,40 @@ public class DBFile {
     public void SetFilePath(String value) {
         filePath = value;
     }
+
+    public String GetFullFilePath() {
+        return DEFAULT_PATH_PREFIX + filePath + label + extension;
+    }
+
+    public String GetData() {
+        return data.toString();
+    }
     // END Getters & Setters -------------------------------
+
+    // Runtime Data I/O ------------------------------------
+    public void WriteData(String toWrite) {
+        data.append(toWrite);
+    }
+    // END Runtime Data I/O --------------------------------
+
+    // File I/O --------------------------------------------
+    public void StoreFile() {
+        try {
+            File toStore = new File(GetFullFilePath());
+            if (toStore.createNewFile()) {
+                System.out.println("Created new file");
+            } else {
+                System.out.println("File exists already");
+            }
+
+            FileWriter os = new FileWriter(toStore);
+            os.write(GetData());
+            os.close();
+        } catch (Exception e) {
+            System.err.println("An error occured");
+            e.printStackTrace();
+        }
+
+    }
+    // END File I/O ----------------------------------------
 }
--- src/test/java/jankdb/JankDBTest.java
+++ src/test/java/jankdb/JankDBTest.java
@@ -1,52 +1,118 @@
 package jankdb;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.*;
+
+import org.junit.jupiter.api.Test;
 
 public class JankDBTest {
     @Test
     public void testDBFileExists() {
         DBFile f = new DBFile();
-        assertNotEquals(null, f);
+        assertNotEquals(null, f, "DBFile: Exists");
     }
 
     @Test
     public void testDBFileHasLabelConstructor() {
         DBFile f = new DBFile("");
-        assertNotEquals(null, f);
+        assertNotEquals(null, f, "DBFile: Has Label Constructor");
     }
 
     @Test
     public void testDBFileHasLabelIsValid() {
         DBFile f = new DBFile("test");
-        assertEquals("test", f.GetLabel());
+        assertEquals("test", f.GetLabel(), "DBFile: Gets Label");
     }
 
     @Test
-    public void testDBFileCanSetLabel(){
+    public void testDBFileCanSetLabel() {
         DBFile f = new DBFile("test");
         assertEquals("test", f.GetLabel());
         f.SetLabel("new");
-        assertEquals("new", f.GetLabel());
+        assertEquals("new", f.GetLabel(), "DBFile: Sets Label");
     }
+
     @Test
     public void testDBFileHasFilePathConstructor() {
         DBFile f = new DBFile("", "");
-        assertNotEquals(null, f);
+        assertNotEquals(null, f, "DBFile: Has File Path Constructor");
     }
 
     @Test
     public void testDBFileHasFilePathIsValid() {
-        DBFile f = new DBFile("test", "file");
-        assertEquals("file", f.GetFilePath());
+        DBFile f = new DBFile("test", "./");
+        assertEquals("./", f.GetFilePath(), "DBFile: Gets File Path");
     }
 
     @Test
-    public void testDBFileCanSetFilePath(){
+    public void testDBFileCanSetFilePath() {
+        DBFile f = new DBFile("test", "./");
+        assertEquals("./", f.GetFilePath());
+        f.SetFilePath("/");
+        assertEquals("/", f.GetFilePath(), "DBFile: Sets File Path");
+    }
+
+    @Test
+    public void testDBFileHasFullFilePathIsValid() {
+        DBFile f = new DBFile("test", "new/");
+        assertEquals("src/main/resources/new/test.txt", f.GetFullFilePath(), "DBFile: Gets Full File path");
+    }
+
+    @Test
+    public void testDBFileaHasData() {
         DBFile f = new DBFile("test", "file");
-        assertEquals("file", f.GetFilePath());
-        f.SetFilePath("new");
-        assertEquals("new", f.GetFilePath());
+        assertEquals("", f.GetData(), "DBFile: Has Data");
+    }
+
+    @Test
+    public void testDBFileaCanWriteData() {
+        DBFile f = new DBFile("test", "file");
+        String dataStr = "Data written to";
+        f.WriteData(dataStr);
+        assertEquals(dataStr, f.GetData(), "DBFile: Can Write Data");
+    }
+
+    @Test
+    public void testDBFileCanStoreFile() {
+        DBFile f = new DBFile("test", "store/");
+        String dataStr = "Data Stored persistently!";
+        f.WriteData(dataStr);
+        f.StoreFile();
+        boolean fileFound = false;
+        try {
+            FileReader fReader = new FileReader(f.GetFullFilePath());
+            fileFound = true;
+            fReader.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            fileFound = false;
+        } finally {
+            assertTrue(fileFound, "DBFile: Has successfuly Stored File at " + f.GetFullFilePath());
+        }
+    }
+
+    @Test
+    public void testDBFileStoresAccurately() {
+        DBFile f = new DBFile("test", "store/");
+        String dataStr = "Data Stored persistently!";
+        f.WriteData(dataStr);
+        f.StoreFile();
+        String fileData = "";
+        // Read the file contents
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(f.GetFullFilePath()));
+            fileData = reader.readLine(); // Reads first line (adjust if multiline)
+            reader.close();
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            assertEquals(dataStr, fileData); // Compare with expected data
+        }
     }
 }

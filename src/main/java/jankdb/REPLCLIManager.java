4281e6370a52f9feeacca657575f9803bbb4cb3f
HShrigma <hristostantchev@abv.bg>
2025-04-18 13:04:27 +0300

    Update REPCLIManager.java, CLICommandRegistry.java - setup skeleton

--- src/main/java/jankdb/REPLCLIManager.java
+++ src/main/java/jankdb/REPLCLIManager.java
@@ -1,8 +1,147 @@
 package jankdb;
 
-public class REPLCLIManager {
-    public void Start(){
+import java.util.Scanner;
 
+import jankdb.helpers.*;
+
+public class REPLCLIManager {
+
+    Table mainTable;
+
+    public REPLCLIManager() {
+        mainTable = new Table("main");
     }
-    
+
+    public void Start() {
+        System.out.println(CLICommandRegistry.Messages.GREETING);
+        mainTable.Load();
+        Run();
+    }
+
+    void Run() {
+        Scanner scanner = new Scanner(System.in);
+        String command = GetCommand(scanner);
+        // run loop by evaluating command
+        while (IsNotExit(command)) {
+            // Parse the command
+            ParseCommand(command);
+            // Get new command
+            command = GetCommand(scanner);
+        }
+
+        scanner.close();
+    }
+
+    String GetCommand(Scanner scanner) {
+        System.err.println();
+        return scanner.nextLine();
+    }
+
+    boolean IsNotExit(String command) {
+        return !command.equals(CLICommandRegistry.BaseCommands.EXIT);
+    }
+
+    void ParseCommand(String command) {
+        String[] split = SplitCommand(command);
+        switch (split[0]) {
+            case CLICommandRegistry.BaseCommands.GET:
+                ProcessGetCommand(split);
+                break;
+            case CLICommandRegistry.BaseCommands.SET:
+                ProcessSetCommand(split);
+                break;
+            case CLICommandRegistry.BaseCommands.DEL:
+                ProcessDeleteCommand(split);
+                break;
+            case CLICommandRegistry.BaseCommands.KEYS:
+                ProcessKeysCommand(split);
+                break;
+            case CLICommandRegistry.BaseCommands.SAVE:
+                ProcessSaveCommand(split);
+                break;
+            case CLICommandRegistry.BaseCommands.CLEAR:
+                ProcessClearCommand(split);
+                break;
+            case CLICommandRegistry.BaseCommands.HELP:
+                ProcessHelpCommand(split);
+                break;
+            case CLICommandRegistry.BaseCommands.EXIT:
+                ProcessExitCommand(split);
+                break;
+            default:
+                break;
+        }
+    }
+
+    String[] SplitCommand(String command) {
+        return command.split(" ");
+    }
+
+    void ProcessGetCommand(String[] splitCommand) {
+        if (IsValidCommandSize(2, splitCommand, CLICommandRegistry.CommandSizeRules.GET)) {
+            // Prints value of key if found
+        }
+    }
+
+    void ProcessSetCommand(String[] splitCommand) {
+        if (IsValidCommandSize(3, splitCommand, CLICommandRegistry.CommandSizeRules.SET)) {
+            // Sets Key to Value if found
+        }
+    }
+
+    void ProcessDeleteCommand(String[] splitCommand) {
+        if (IsValidCommandSize(2, splitCommand, CLICommandRegistry.CommandSizeRules.DEL)) {
+            // Deletes Key if found
+        }
+    }
+
+    void ProcessKeysCommand(String[] splitCommand) {
+        if (IsValidCommandSize(1, splitCommand, CLICommandRegistry.CommandSizeRules.KEYS)) {
+            // Lists all Keys
+        }
+    }
+
+    void ProcessSaveCommand(String[] splitCommand) {
+        if (IsValidCommandSize(1, splitCommand, CLICommandRegistry.CommandSizeRules.SAVE)) {
+            // Saves DB to persistent storage
+        }
+    }
+
+    void ProcessClearCommand(String[] splitCommand) {
+        if (IsValidCommandSize(1, splitCommand, CLICommandRegistry.CommandSizeRules.CLEAR)) {
+            // Clears All Table Items
+            
+        }
+    }
+
+    void ProcessHelpCommand(String[] splitCommand) {
+        if (IsValidCommandSize(1, splitCommand, CLICommandRegistry.CommandSizeRules.HELP)) {
+            // prints All command Guidelines
+            System.err.println(CLICommandRegistry.CommandGuides.GET);
+            System.err.println(CLICommandRegistry.CommandGuides.SET);
+            System.err.println(CLICommandRegistry.CommandGuides.DEL);
+            System.err.println(CLICommandRegistry.CommandGuides.KEYS);
+            System.err.println(CLICommandRegistry.CommandGuides.SAVE);
+            System.err.println(CLICommandRegistry.CommandGuides.CLEAR);
+            System.err.println(CLICommandRegistry.CommandGuides.HELP);
+            System.err.println(CLICommandRegistry.CommandGuides.EXIT);
+        }
+    }
+
+    void ProcessExitCommand(String[] splitCommand) {
+        if (IsValidCommandSize(1, splitCommand, CLICommandRegistry.CommandSizeRules.EXIT)) {
+            // prints goodbye
+            System.out.println(CLICommandRegistry.Messages.GOODBYE);
+        }
+    }
+
+    boolean IsValidCommandSize(int expectedSize, String[] arr, String formatMSG) {
+        if (arr.length == expectedSize)
+            return true;
+
+        System.err.println(CLICommandRegistry.Messages.INVALID_SIZE_ERR);
+        System.out.println(formatMSG);
+
+        return false;
+    }
 }
--- src/main/java/jankdb/helpers/CLICommandRegistry.java
+++ src/main/java/jankdb/helpers/CLICommandRegistry.java
@@ -16,4 +16,32 @@ public final class CLICommandRegistry {
         public static final String HELP = "HELP";
         public static final String EXIT = "EXIT";
     }
+
+    public static final class Messages {
+        public static final String GREETING = "Welcome to JankDB!";
+        public static final String GOODBYE = "Thank you for using JankDB!\nThe program will now exit";
+        public static final String REQ_COMMAND = "Please enter a command:\n(use command \"HELP\" to get a list of commands)";
+        public static final String INVALID_SIZE_ERR = "Invalid size of command!";
+    }
+
+    public static final class CommandSizeRules{
+        public static final String SET = "SET must have 2 arguments formatted as: SET <KEY> <VALUE>";
+        public static final String GET = "GET must have 1 argument formatted as: GET <KEY>";
+        public static final String DEL = "DEL must have 1 argument formatted as: DEL <KEY>";
+        public static final String KEYS = "KEYS has no arguments!";
+        public static final String SAVE = "SAVE has no arguments!";
+        public static final String CLEAR = "CLEAR has no arguments!";
+        public static final String HELP = "HELP has no arguments!";
+        public static final String EXIT = "EXIT has no arguments!";
+    }
+    public static final class CommandGuides{
+        public static final String SET = "SET updates the <Key> value to <Value> (if found).";
+        public static final String GET = "GET returns the <Key> value (if found).";
+        public static final String DEL = "DEL deletes the associated <Key>:Value pair (if found).";
+        public static final String KEYS = "KEYS returns all keys in the database.";
+        public static final String SAVE = "SAVE saves all current operations to persistent storage.";
+        public static final String CLEAR = "CLEAR removes all entries in the database.";
+        public static final String HELP = "HELP lists all available actions.";
+        public static final String EXIT = "EXIT terminates the current session.";
+    }
 }
